87a65c8805a1f9b6b8b28118c381304db81df21a
dynamic-daemon-637b <zl3087@columbia.edu>
2021-07-03 00:54:51 +0000

    r

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/' do
+erb:index
+end
 
+get '/new' do
+erb :create_puppy
+end
+
+post '/puppy' do
+  p1=Puppy.new
+  @puppy= p1.inparams[:name, :breed,:age]
+ 
+  erb :display_puppy
+end
 end
